f072c57249fac4866fe32f212b52e5a5e1cce8c7
superkang <superkang@superkangs-MacBook-Air.local>
2022-06-20 15:13:07 -0400

    ship movement modified

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,7 +7,10 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_height = self.screen.get_rect().height
+        self.settings.screen_width = self.screen.get_rect().width
+        #self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Andy's Game")
 
         self.ship = Ship(self)
@@ -21,24 +24,31 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
-                    elif event.key == pygame.K_UP:
-                        self.ship.moving_up = True
-                    elif event.key == pygame.K_DOWN:
-                        self.ship.moving_down = True
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
+                    self._check_keyup_events(event)
+            
+    def _check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+                        self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+                        self.ship.moving_down = True
+        elif event.key == pygame.K_ESCAPE:
+              sys.exit()
+    def _check_keyup_events(self,event):
+        if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = False
-                    elif event.key == pygame.K_UP:
+        elif event.key == pygame.K_UP:
                         self.ship.moving_up = False
-                    elif event.key == pygame.K_DOWN:
+        elif event.key == pygame.K_DOWN:
                         self.ship.moving_down = False
-
+    
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
--- ship.py
+++ ship.py
@@ -9,22 +9,22 @@ class Ship:
         self.image = pygame.image.load('images/ship.bmp')
         #self.image= pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
-
+        
         self.rect.midbottom = self.screen_rect.midbottom
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.x += 5
-        if self.moving_left:
+        if self.moving_left and self.rect.left > self.screen_rect.left:
             self.rect.x -= 5
-        if self.moving_up:
+        if self.moving_up and self.rect.top > self.screen_rect.top:
             self.rect.y -= 5
-        if self.moving_down:
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.rect.y += 5
-        
+
     
     def blitme(self):
         self.screen.blit(self.image,self.rect)
