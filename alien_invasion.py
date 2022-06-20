73d676334369a63c1cf9d28ab7774feb39f59dff
superkang <superkang@superkangs-MacBook-Air.local>
2022-06-20 14:12:34 -0400

    modified ship movements

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+from xmlrpc.client import TRANSPORT_ERROR
 import pygame
 from ship import *
 from setting import Settings
@@ -12,13 +13,37 @@ class AlienInvasion:
         self.ship = Ship(self)
     def run_game(self):
         while True:
-            for event in pygame.event.get():
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            pygame.display.flip()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                    elif event.key == pygame.K_UP:
+                        self.ship.moving_up = True
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.moving_down = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                    elif event.key == pygame.K_UP:
+                        self.ship.moving_up = False
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.moving_down = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+   
+        pygame.display.flip()
 if __name__ == "__main__":
   ai = AlienInvasion()
   ai.run_game()
--- ship.py
+++ ship.py
@@ -7,10 +7,24 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')
+        #self.image= pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 5
+        if self.moving_left:
+            self.rect.x -= 5
+        if self.moving_up:
+            self.rect.y -= 5
+        if self.moving_down:
+            self.rect.y += 5
+        
     
     def blitme(self):
         self.screen.blit(self.image,self.rect)
-        
