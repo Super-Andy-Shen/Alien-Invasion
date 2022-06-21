4172623b34c57c13d4bdfe40cf4f8264f6cfb056
superkang <superkang@superkangs-MacBook-Air.local>
2022-06-21 11:57:02 -0400

    added bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,22 +3,32 @@ from xmlrpc.client import TRANSPORT_ERROR
 import pygame
 from ship import *
 from setting import Settings
+from bullet import Bullet
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-        self.settings.screen_height = self.screen.get_rect().height
-        self.settings.screen_width = self.screen.get_rect().width
-        #self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+       # self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        #self.settings.screen_height = self.screen.get_rect().height
+        #self.settings.screen_width = self.screen.get_rect().width
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Andy's Game")
-
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+    
+    
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _check_events(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -39,6 +49,12 @@ class AlienInvasion:
                         self.ship.moving_down = True
         elif event.key == pygame.K_ESCAPE:
               sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+    def _fire_bullet(self):
+        if len(self.bullets) < 10:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
     def _check_keyup_events(self,event):
         if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
@@ -52,6 +68,8 @@ class AlienInvasion:
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
    
         pygame.display.flip()
 if __name__ == "__main__":
--- setting.py
+++ setting.py
@@ -4,4 +4,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-        self.ship_speed = 5
+        
+        self.bullet_speed = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (200,60,60)
+        #self.ship_speed = 5
